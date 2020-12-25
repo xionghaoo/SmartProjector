e33d70fb2371532f75163d756d7d555eedecdfd1
zero <xionghao930316@gmail.com>
2020-12-25 15:07:46 +0800

    视频全屏，本地视频播放

--- app/src/main/java/com/ubtrobot/smartprojector/ui/VideoActivity.kt
+++ app/src/main/java/com/ubtrobot/smartprojector/ui/VideoActivity.kt
@@ -1,16 +1,20 @@
 package com.ubtrobot.smartprojector.ui
 
 import android.Manifest
-import android.net.Uri
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.Environment
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.ubtrobot.smartprojector.R
-import com.ubtrobot.smartprojector.utils.ToastUtil
 import kotlinx.android.synthetic.main.activity_video.*
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
@@ -18,11 +22,15 @@ import timber.log.Timber
 import java.io.File
 
 
-class VideoActivity : AppCompatActivity() {
+class VideoActivity : AppCompatActivity(), Player.EventListener {
 
     companion object {
         private const val RC_STORAGE_PERMISSION = 1
     }
+
+    private var player: SimpleExoPlayer? = null
+    private var btnFullscreen: ImageView? = null
+    private var isFullscreen: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,33 +38,112 @@ class VideoActivity : AppCompatActivity() {
         videoPlayTask()
     }
 
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        player?.release()
+        super.onDestroy()
+    }
+
+//    override fun onConfigurationChanged(newConfig: Configuration) {
+//        super.onConfigurationChanged(newConfig)
+//
+//        // Checks the orientation of the screen
+//        if (newConfig.orientation === Configuration.ORIENTATION_LANDSCAPE) {
+//            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show()
+//        } else if (newConfig.orientation === Configuration.ORIENTATION_PORTRAIT) {
+//            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
     @AfterPermissionGranted(RC_STORAGE_PERMISSION)
     private fun videoPlayTask() {
         if (hasStoragePermission()) {
-            Timber.d("has permission")
-            val player = SimpleExoPlayer.Builder(this)
-                .build()
-            player_view.player = player
-
-            val testVideo = "https://storage.googleapis.com/wvmedia/clear/hevc/tears/tears.mpd"
-            val path = File(Environment.getExternalStorageDirectory(), "${Environment.DIRECTORY_DOWNLOADS}/video1.mp4")
-            Timber.d("download path: ${path}")
-            val item = MediaItem.fromUri(path.toUri())
-            player.setMediaItem(item)
-            player.prepare()
-            player.play()
-
-            Glide.with(this)
-                .load(path.toUri())
-                .centerCrop()
-                .into(iv_thumbnail)
+            initialPlayer()
         } else {
             EasyPermissions.requestPermissions(
                 this,
                 "App需要读取外置存储卡，请授予权限",
                 RC_STORAGE_PERMISSION,
-                Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
             )
+        }
+    }
+
+    private fun initialPlayer() {
+        player = SimpleExoPlayer.Builder(applicationContext).build()
+
+        isFullscreen = false
+        btnFullscreen = player_view.findViewById(R.id.exo_fullscreen_icon)
+        btnFullscreen?.setOnClickListener {
+            if (isFullscreen) {
+                // 全屏状态, 退出全屏
+                btnFullscreen?.setImageResource(R.drawable.ic_fullscreen_open)
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+                supportActionBar?.show()
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                val params = player_view.layoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                params.height = resources.getDimension(R.dimen.player_normal_height).toInt()
+                player_view.layoutParams = params
+                isFullscreen = false
+            } else {
+                // 正常状态， 开启全屏
+                btnFullscreen?.setImageResource(R.drawable.ic_fullscreen_close)
+                window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+                supportActionBar?.hide()
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                val params = player_view.layoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                params.height = RelativeLayout.LayoutParams.MATCH_PARENT
+                player_view.layoutParams = params
+                isFullscreen = true
+            }
+        }
+
+        player!!.addListener(this)
+
+        player_view.player = player
+
+        val testVideo = "https://storage.googleapis.com/wvmedia/clear/hevc/tears/tears.mpd"
+        val video1 = File(
+            Environment.getExternalStorageDirectory(),
+            "${Environment.DIRECTORY_DOWNLOADS}/The Angry Birds Movie.mp4"
+        )
+        val video2 = File(
+            Environment.getExternalStorageDirectory(),
+            "${Environment.DIRECTORY_DOWNLOADS}/video1.mp4"
+        )
+        Timber.d("download path: ${video1}")
+        val item1 = MediaItem.fromUri(video1.toUri())
+        val item2 = MediaItem.fromUri(video2.toUri())
+        player!!.addMediaItem(item1)
+        player!!.addMediaItem(item2)
+        player!!.prepare()
+
+        Glide.with(this)
+            .load(video1.toUri())
+            .centerCrop()
+            .into(iv_video_1)
+
+        Glide.with(this)
+            .load(video2.toUri())
+            .centerCrop()
+            .into(iv_video_2)
+
+        iv_video_1.setOnClickListener {
+            player?.seekTo(0, 0)
+            player!!.playWhenReady = true
+        }
+
+        iv_video_2.setOnClickListener {
+            player?.seekTo(1, 0)
+            player?.playWhenReady = true
         }
     }
 
@@ -70,7 +157,11 @@ class VideoActivity : AppCompatActivity() {
     }
 
     private fun hasStoragePermission() : Boolean {
-        return EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return EasyPermissions.hasPermissions(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
     }
 
 //    // permission callback
@@ -93,4 +184,8 @@ class VideoActivity : AppCompatActivity() {
 //    override fun onRationaleDenied(requestCode: Int) {
 //        Timber.d( "onRationaleDenied: ${requestCode}")
 //    }
+
+    override fun onPlaybackStateChanged(state: Int) {
+
+    }
 }
