ae8dbfcbeeb42b95e41d9377ce593bf44e2d04d7
zero <xionghao930316@gmail.com>
2021-01-27 11:10:05 +0800

    jni lib test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,10 +2,16 @@
 // Created by ubtech on 2021/1/26.
 //
 
-#include <string.h>
+#include <iostream>
+#include <string>
 #include <jni.h>
+#include <gperf.h>
+
+using namespace std;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ubtrobot_smartprojector_ui_settings_SettingsFragment_helloStr(JNIEnv* env, jobject thiz) {
-    return env->NewStringUTF("Hello JNI");
+    auto ticks = GetTicks();
+    auto str = "Hello JNI, " + to_string(ticks);
+    return env->NewStringUTF(str.c_str());
 }
