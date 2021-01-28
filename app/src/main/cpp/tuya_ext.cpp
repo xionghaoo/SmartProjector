398c499ac0f14f34d0065a1fba95d602323626e8
zero <xionghao930316@gmail.com>
2021-01-28 16:02:47 +0800

    jni log

--- app/src/main/cpp/tuya_ext.cpp
+++ app/src/main/cpp/tuya_ext.cpp
@@ -5,10 +5,21 @@
 #include <iostream>
 #include <string>
 #include <jni.h>
+#include <android/log.h>
 // 测试
 #include <gperf.h>
 
 #include <tuya_gw_infra_api.h>
+
+// 定义日志类型
+#ifndef LOG_TAG
+#define LOG_TAG "Tuya_Ext"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG ,__VA_ARGS__) // 定义LOGD类型
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG ,__VA_ARGS__) // 定义LOGI类型
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,LOG_TAG ,__VA_ARGS__) // 定义LOGW类型
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG ,__VA_ARGS__) // 定义LOGE类型
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,LOG_TAG ,__VA_ARGS__) // 定义LOGF类型
+#endif
 
 /* must apply for uuid, authkey and product key from tuya iot develop platform */
 #define UUID         "003tuyatestf7f149185"
@@ -23,6 +34,7 @@ using namespace std;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ubtrobot_smartprojector_ui_settings_SettingsFragment_helloStr(JNIEnv* env, jobject thiz) {
+    LOGD("log form jni");
     ty_gw_attr_s gw_attr = {
             .storage_path = "./",
             .cache_path = "/tmp/",
